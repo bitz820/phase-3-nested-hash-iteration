c311578920357ae1780651983442f867d6f9752a
Mark <mark.n.bacon.jr@gmail.com>
2022-04-24 17:51:11 -0400

    commit tree c790e751fc0c0a5663d02844ef9a7caf7cb67900

--- contacts.rb
+++ contacts.rb
@@ -18,8 +18,28 @@ def contacts
 end
 
 def remove_strawberry(contacts)
-  # your code here!
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
+  end
 end
+#  contacts.each do |person, data|
+ #   if person == "Freddy Mercury"
+  #    data.each do |attribute, value|
+#        if attribute == favorite_ice_cream_flavors
+ #         if value.include? "strawberry"
+  #          binding.pry
+   #     end
+#      end
+ #   end
+    #person.each do |name
+ # end
+#end
 
 # print the output to the terminal for inspection
 pp remove_strawberry(contacts)
